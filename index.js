5f49cb66f1e6059c6d48fcc1cc825e148884c9a0
Redrogue12 <exgc12@gmail.com>
2018-12-22 22:14:01 -0400

    Added port env variable

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.post('/subscribe', (req, res) => {
   webpush.sendNotification(subscription, payload,).catch(err => console.error(err));
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
